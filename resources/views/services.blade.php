f9c852780c307dd2e72e10cfff805e792179726d
tajulislam92 <tj.career2014@gmail.com>
2022-10-02 22:55:11 +0600

    Conditional rendering

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -3,5 +3,9 @@
 @section('content')
 <div class="page-title">
     <h1>Services Page</h1>
+    @for ($i=0;$i<4;$i++)
+        {{ $services_1[$i] }}<br>
+
+    @endfor
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
 Route::get('/about-us', function () {
     return view('about',[
     'page_name' => 'About Page',
     'name'=>'Laravel 9'
     ]);
 })->name('about');
+
 Route::get('/contact-us', function () {
 $page_name= "Contact Page";
 $mobile = "01918300000";
     return view('contact', compact('page_name','mobile'));
 })->name('contact');
+
 Route::get('/services', function () {
-    return view('services');
+
+    $services_1 = [
+    'Web Design',
+    'Web Development',
+    'App Development',
+    'Graphics Design',
+    ];
+
+    return view('services', compact('services_1'));
+
 })->name('service');
+
 Route::get('/test-page', function () {
     return view('test');
 })->name('test');
